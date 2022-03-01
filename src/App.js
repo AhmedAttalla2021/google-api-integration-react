21d222ca63286393d47aad14e306df854ca2b6fb
AhmedAttalla2021 <ahmedbebo9410@gmail.com>
2022-03-01 20:12:02 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-import { GoogleLogin } from "react-google-login";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 function App() {
   const clientId =
     "159130820163-u8bn9fr1cm6hqe55o2vqvpvqga3vpv62.apps.googleusercontent.com";
-
+  const [showLoginBtn, setShowLoginBtn] = useState(true);
   const loginSuccess = (res) => {
     console.log("login success", res.profileObj);
+    setShowLoginBtn(false);
   }; // on login success function
 
   const loginFailure = (res) => {
     console.log("login fail", res);
   }; // on login failure function
+
+  const logoutSuccess = () => {
+    console.log("logout success");
+    setShowLoginBtn(true);
+  };
   return (
-    <GoogleLogin
-      clientId={clientId}
-      buttonText="Login"
-      onSuccess={loginSuccess}
-      onFailure={loginFailure}
-      cookiePolicy={"single_host_origin"}
-    />
+    <div className="login-logout-btn">
+      {showLoginBtn ? (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Login"
+          onSuccess={loginSuccess}
+          onFailure={loginFailure}
+          cookiePolicy={"single_host_origin"}
+        />
+      ) : (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText="Logout"
+          onLogoutSuccess={logoutSuccess}
+        ></GoogleLogout>
+      )}
+    </div>
   );
 }
 
